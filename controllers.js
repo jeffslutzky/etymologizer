3bd2760c098cc41e36db9bc2d257f831cb0c3bfb
jeffslutzky <jeffslutzky@gmail.com>
2016-04-20 23:17:01 -0400

    work on parsing series of words

--- controllers.js
+++ controllers.js
@@ -1,3 +1,4 @@
+
 myApp.controller('homeController', ['$scope', 'textService', function($scope, textService) {
 
     $scope.text = textService.text;
@@ -10,24 +11,29 @@ myApp.controller('homeController', ['$scope', 'textService', function($scope, te
 
 
 myApp.controller('resultController', ['$scope', '$http', '$resource', 'textService', function($scope, $http, $resource, textService) {
-
   $scope.text = textService.text;
 
-  $scope.etymology = "";
+  $scope.$watch('text', function() {
+     textService.text = $scope.text;
+  });
 
-  $scope.click = function (){
-    console.log("Clicked");
-  };
+  $scope.etymology = "";
 
   $scope.resourceURL = "http://localhost:3000/api/word";
 
   $http.get($scope.resourceURL, {
-    params: {word: $scope.text}
+    params: {words: $scope.text}
   })
     .then(function success(response) {
       console.log(response);
+// cycle through each word
+  for (var word in response.data) {
+    debugger;
+  };
+
       if (response.data.entry_list.entry) {
         $scope.etymology = response.data.entry_list.entry[0] ? response.data.entry_list.entry[0].et : response.data.entry_list.entry.et;
+
       console.log($scope.text + ": " + $scope.etymology);
     }}, function error(response) {
       console.log("Error");
