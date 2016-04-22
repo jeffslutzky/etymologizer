9542474bf2a96056fca2c1af398d9feec2033b3a
jeffslutzky <jeffslutzky@gmail.com>
2016-04-22 15:59:47 -0400

    display typed word instead of dictionary variant

--- controllers.js
+++ controllers.js
@@ -7,34 +7,23 @@ myApp.controller('homeController', ['$scope', '$http', '$resource', 'textService
      textService.text = $scope.text;
   });
 
-
   $scope.resourceURL = "http://localhost:3000/api/word";
 
   $scope.showEtymology = function() {
 
+    $scope.words = $scope.text.split(" ");
     $scope.etymology = [];
 
     $http.get($scope.resourceURL, {
       params: {words: $scope.text}
     })
       .then(function success(response) {
-
         // cycle through each word
         $.each(response.data, function() {
-          if (this.entry_list.entry) { // if word exists
-            entry = this.entry_list.entry;
-            var word = entry[0] ? entry[0].ew : entry.ew;
-
-            if (entry[0]) {
-              $scope.wordEtymology = etymologyService.findEtymology();
-            } else {
-              $scope.wordEtymology = entry.et;
-            };
-
-            $scope.etymology.push({"word": word, "etymology": $scope.wordEtymology});
-          };
+          word = $scope.words.shift()
+          wordEtymology = etymologyService.findEtymology(this);
+          $scope.etymology.push({"word": word, "etymology": wordEtymology});
         });
-
       }, function error(response) {
         console.log("Error");
       });
--- services.js
+++ services.js
@@ -7,11 +7,23 @@ myApp.service('textService', function(){
 
 myApp.service('etymologyService', function(){
 
-  this.findEtymology = function() {
+  this.findEtymology = function(word) {
+
+    if (word.entry_list.entry) { // if word is a real word
+      entry = word.entry_list.entry;
+      if (entry.length) {
+        return this.findCorrectSubEntry();
+      } else {
+        return entry.et;
+      };
+    };
+  };
+
+  this.findCorrectSubEntry = function() {
     for (i = 0; i < entry.length; i++) {
       if (entry[i].et) {
         return entry[i].et;
-      }
+      };
     };
   };
 
